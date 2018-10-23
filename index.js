96aabbffb6275090b31ed1939f86eb810fc9cd15
Admin <admin@Ngos-MacBook-Pro.local>
2018-10-23 09:49:45 +0700

    lan1

---
+++ index.js
@@ -0,0 +1,11 @@
+//tao web server
+
+const express = require('express');
+const app = express();
+app.listen(3000,() => console.log('Server Stared.'));
+
+/*
+const express = require('express')
+const app =  express();
+app.listen(3000, callbackFunction)
+*/
